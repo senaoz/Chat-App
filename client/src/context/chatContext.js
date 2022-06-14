13477587854b9a25efe70c827d4f8d8903963738
Sena Oz <tazeyta@gmail.com>
2022-06-14 22:57:22 +0300

    14 June 2022

--- client/src/context/chatContext.js
+++ client/src/context/chatContext.js
@@ -1,3 +1,12 @@
 import { createContext, useState } from "react";
 
-const ChatContext =
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [chat, setChat] = useState([]);
+  const values = {};
+
+  return (
+    <ChatContext.Provider values={values}>{children}</ChatContext.Provider>
+  );
+};
